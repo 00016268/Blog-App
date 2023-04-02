b4ded1d1d20457e562dff72a5d32c7317d23ebb5
00016268 <wiut16268@gmail.com>
2023-04-03 02:10:08 +0500

    created read operation for a single blog

--- app.js
+++ app.js
@@ -58,8 +58,23 @@ app.get('/blogs', (req, res) => {
    
 })
 
-app.get('/blogs/detail', (req,res) => {
-    res.render('detail')
+app.get('/blogs/:id', (req,res) => {
+
+    const id = req.params.id
+
+    fs.readFile('./data/blogs.json', (err, data) => {
+        if (err) throw err
+
+        const blogs = JSON.parse(data)
+
+        const blog = blogs.filter(blog => blog.id == id) [0]
+
+        res.render('detail', {blog: blog})
+    })
+
+
+
+   
 
 })
 
